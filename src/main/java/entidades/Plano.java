015d53d6b69e5a17324a89820aa23c725df7ae31
HILL46 <gustavoriderplay@gmail.com>
2026-05-07 18:04:31 -0300

    Plano.java

--- src/main/java/entidades/Plano.java
+++ src/main/java/entidades/Plano.java
@@ -1,4 +1,43 @@
 package entidades;
 
 public class Plano {
+
+    private int id;
+    private String nome;
+    private double valor;
+
+    public Plano(int id, String nome, double valor) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Plano{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", valor=" + valor +
+                '}';
+    }
 }
